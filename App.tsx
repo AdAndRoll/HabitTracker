16356c344d585b55ccfb5a2aa12453696b3f2490
AdAndRoll <vanovano03@mail.ru>
2026-04-02 00:17:00 +0300

    final refractor

--- App.tsx
+++ App.tsx
@@ -11,7 +11,6 @@ const App = () => {
   const activeTheme = isDarkMode ? darkTheme : lightTheme;
 
   useEffect(() => {
-    // Инициализация происходит строго после загрузки основного контекста React
     initCalendarLocale();
   }, []);
 
